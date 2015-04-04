db1b7c9b920566b106eb35ae1f8cdadfbc47c161
Ahmad M Abdelrahman <ahmad18189@gmail.com>
2015-04-04 14:45:57 +0300

    First Commit

---
+++ Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScenes : MonoBehaviour {
+
+	public void GoToScene(string scene){
+		Application.LoadLevel(scene);
+	}
+	public void ClickTest (string a)
+	{
+		Debug.Log ( a);
+	}
+}
