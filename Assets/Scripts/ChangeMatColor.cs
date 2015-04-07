51168e7cf2e671b729befdf682a450a59e7634e8
Ahmad M Abdelrahman <ahmad18189@gmail.com>
2015-04-07 13:43:55 +0300

    Last Save
    FAST not Finished

---
+++ Assets/Scripts/ChangeMatColor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeMatColor : MonoBehaviour {
+public void SetMatirialColor(GameObject go ) {
+		go.GetComponent<Image>().color = Color.blue;
+	}
+}
---
+++ Assets/Scripts/PlayVideos.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVideos : MonoBehaviour {
+
+	public void StartVideo () {
+		Handheld.PlayFullScreenMovie ("What Is A Stroke.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+	}
+}
