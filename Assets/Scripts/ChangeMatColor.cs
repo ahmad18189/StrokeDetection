3c03d332b293df52db49e1f83824dffaa7e956b6
Ahmad M Abdelrahman <ahmad18189@gmail.com>
2015-04-08 12:15:57 +0300

    REd BLue Fast

--- Assets/Scripts/ChangeMatColor.cs
+++ Assets/Scripts/ChangeMatColor.cs
@@ -3,7 +3,15 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class ChangeMatColor : MonoBehaviour {
-public void SetMatirialColor(GameObject go ) {
-		go.GetComponent<Image>().color = Color.blue;
+	public GameObject [] MyButtons ;
+
+
+	public void SetMatirialColorRed(GameObject go) {
+		go.GetComponent<Image> ().color = Color.red;
+	}
+	public void SetMatirialColorBlue(GameObject go) {
+		foreach (GameObject asd in MyButtons)
+			SetMatirialColorRed(asd);
+		go.GetComponent<Image> ().color = Color.blue;
 	}
 }
