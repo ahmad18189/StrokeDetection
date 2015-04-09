3ba7322b2c7a1224a6dfaf707c02912752707703
Ahmad M Abdelrahman <ahmad18189@gmail.com>
2015-04-09 13:40:49 +0300

    Revert "REd BLue Fast"
    This reverts commit 3c03d332b293df52db49e1f83824dffaa7e956b6.
    
    Conflicts:
    	Library/CurrentMaximizeLayout.dwlt
    	Library/assetDatabase3
    	Library/metadata/00/00000000000000004000000000000000

--- Assets/Scripts/ChangeMatColor.cs
+++ Assets/Scripts/ChangeMatColor.cs
@@ -3,15 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class ChangeMatColor : MonoBehaviour {
-	public GameObject [] MyButtons ;
-
-
-	public void SetMatirialColorRed(GameObject go) {
-		go.GetComponent<Image> ().color = Color.red;
-	}
-	public void SetMatirialColorBlue(GameObject go) {
-		foreach (GameObject asd in MyButtons)
-			SetMatirialColorRed(asd);
-		go.GetComponent<Image> ().color = Color.blue;
+public void SetMatirialColor(GameObject go ) {
+		go.GetComponent<Image>().color = Color.blue;
 	}
 }
