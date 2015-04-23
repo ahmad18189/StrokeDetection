a3291486b5bff531f7161f70dab73fda28e2ed8e
Ahmad M Abdelrahman <ahmad18189@gmail.com>
2015-04-23 20:53:51 +0300

    New!!

---
+++ Assets/Scripts/AnimationCtrl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationCtrl : MonoBehaviour {
+	public GameObject PS;
+	public GameObject rock;
+	void Awake(){
+		Debug.Log ("Awake" );
+		rock.SetActive (false);
+		PS.SetActive (false);
+	
+	}
+	public void AnimationEnded () {
+		Debug.Log ("PEntered" );
+		rock.SetActive (true);
+		PS.SetActive (true);
+	}
+}
