9e821daf878ddc666926a28ad5490487961709f8
Ahmad M Abdelrahman <ahmad18189@gmail.com>
2015-04-23 18:45:50 +0300

    Alot of Fix!

--- Assets/Scripts/PlayVideos.cs
+++ Assets/Scripts/PlayVideos.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class PlayVideos : MonoBehaviour {
 
 	public void StartVideo () {
+		#if UNITY_ANDROID
 		Handheld.PlayFullScreenMovie ("What Is A Stroke.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+		#endif
 	}
 }
